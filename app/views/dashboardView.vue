2f58fdcf540854ad17da6fe6edc84bc85a787ca2
Phyks (Lucas Verney) <phyks@phyks.me>
2016-11-04 16:44:42 -0400

    Beginning of markup

--- app/views/dashboardView.vue
+++ app/views/dashboardView.vue
@@ -1,4 +1,30 @@
 <template lang="pug">
   div
-    p {{ 'Hello world 2' }}
+    h2 {{ 'My RSVPs' }}
+
+    ul
+      li
+        button {{ 'New 15 mins meeting' }}
+      li
+        button {{ 'New 30 mins meeting' }}
+      li
+        button {{ 'New 60 mins meeting' }}
+      li
+        button {{ 'New meeting' }}
+
+    ul
+      li
+        h2 {{ 'Tuesday, November 8, 2016' }}
+        table
+          tr
+            td {{ '09:30am - 09:45am' }}
+            td
+              a(href='mailto:phyks@example.org') {{ 'Phyks' }}
+              span {{ ' with you' }}
+              br
+              span {{ '(15 minutes)' }}
+            td
+              select
+                option {{ 'Accept' }}
+                option {{ 'Discard' }}
 </template>
